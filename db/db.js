ee302123d76087e70618cd97996562c2fb282a57
JJ <jj.vega86@gmail.com>
2021-08-19 11:18:14 -0400

    Added nodemon to project

--- db/db.js
+++ db/db.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://jjvega86:UMsL201vRv75iYIm@cluster0.nm4by.mongodb.net/grading?retryWrites=true&w=majority",
-      { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
-    );
+    await mongoose.connect(process.env.CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
     console.log(`Connected to MongoDb...`);
   } catch (err) {
     console.log(`MongoDb connection failed: ${err}`);
