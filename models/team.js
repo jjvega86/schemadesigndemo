0278f66cf3538ad2a157f4c5ae619ae4b59e0bd5
JJ <jj.vega86@gmail.com>
2021-08-19 20:43:51 -0400

    Added GET query logic to teams.js GET route

--- models/team.js
+++ models/team.js
@@ -1,10 +1,18 @@
 const mongoose = require("mongoose");
+const Joi = require("joi");
+//TODO: bring in Joi for validation on playerSchema and teamSchema and export functions https://gist.github.com/stongo/6359042
 
 const playerSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   rating: { type: Number, required: true },
 });
+
+playerSchema.methods.playerValidate = (player) => {
+  const schema = Joi.object({
+    //TODO: Fill in properties and return schema.validate(player)
+  });
+};
 
 const teamSchema = new mongoose.Schema({
   name: { type: String, required: true },
