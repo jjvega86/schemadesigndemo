ef9600cd14a4445ed154235236c3716a10fc1df1
JJ <jj.vega86@gmail.com>
2021-08-29 12:07:21 -0400

    Modified model/schemas, added two endpoints to comments.js

--- routes/comments.js
+++ routes/comments.js
@@ -4,7 +4,9 @@ const { Comment } = require("../models/comment");
 const { Reply } = require("../models/reply");
 
 //* All Comments endpoints
-//TODO: Add get all comments by videoId
+
+// get all comments by videoId
+// TESTED SUCCESS
 router.get("/:videoId", async (req, res) => {
   try {
     let comments = await Comment.find({ videoId: req.params.videoId });
@@ -17,7 +19,45 @@ router.get("/:videoId", async (req, res) => {
     return res.status(500).send(`Internal Server Error: ${error}`);
   }
 });
-//TODO: Add post comment by videoId
-//TODO: Add post reply by commentId
+
+// Add post comment (use videoId in body of request)
+// TESTED SUCCESS
+router.post("/", async (req, res) => {
+  try {
+    let data = {
+      videoId: req.body.videoId,
+      text: req.body.text,
+      like: 0,
+      dislike: 0,
+      replies: [],
+    };
+
+    let newComment = new Comment(data);
+    let { error } = newComment.commentValidate(req.body);
+    if (error) return res.status(400).send(`Body not valid: ${error}`);
+    await newComment.save();
+    return res.send(newComment);
+  } catch (error) {
+    return res.status(500).send(`Internal Server Error: ${error}`);
+  }
+});
+
+//TODO: Add post reply by commentId and test
+router.post("/:commentId/reply", async (req, res) => {
+  try {
+    let data = {
+      commentId: req.params.commentId,
+      text: req.body.text,
+    };
+
+    let newReply = new Reply(data);
+    let { error } = newReply.replyValidate(req.body);
+    if (error) return res.status(400).send(`Body not valid: ${error}`);
+    await newReply.save();
+    return res.send(newComment);
+  } catch (error) {
+    return res.status(500).send(`Internal Server Error: ${error}`);
+  }
+});
 
 module.exports = router;
